e4d32d5da1f9411059e818eb13a2ee857fd67a02
telix99 <wilson.s.lau@gmail.com>
2026-02-10 05:22:30 +0000

    init 3

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     team = models.CharField(max_length=50)
@@ -8,12 +9,14 @@ class User(models.Model):
         return self.name
 
 class Team(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
     def __str__(self):
         return self.name
 
 class Activity(models.Model):
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=50)
     duration = models.IntegerField()
@@ -22,12 +25,14 @@ class Activity(models.Model):
         return f"{self.user.name} - {self.type}"
 
 class Leaderboard(models.Model):
+    id = models.AutoField(primary_key=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     points = models.IntegerField()
     def __str__(self):
         return f"{self.team.name} - {self.points}"
 
 class Workout(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.CharField(max_length=50)
